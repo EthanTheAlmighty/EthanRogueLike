0e433d79c3e8b10a71eb0ed46d339e32e17847b8
svc-student <svc-student@student.directory.jccc>
2017-03-30 18:59:52 -0500

    finished classwork 3/33

--- LearningTileSetsForRogue/Assets/Scripts/LoadLevel.cs
+++ LearningTileSetsForRogue/Assets/Scripts/LoadLevel.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 using System.Xml;
 
 public class LoadLevel : MonoBehaviour {
+    const string BACKGROUND_FLOOR = "Background Floor";
+    const string BACKGROUND = "Background";
+    const string BACKGROUND_DETAIL = "Background Detail";
+    const string OBSTACLE = "Obstacle";
+    const string INTERACTIBLE = "Interactible";
+    const string PLAYER = "Player";
+    const string FOREGROUND = "Foreground";
+    const string FOREGROUND_DETAIL = "Foreground Detail";
 
     // Use this for initialization
     void Start()
@@ -12,36 +20,67 @@ public class LoadLevel : MonoBehaviour {
         TextAsset levelinfo;
         levelinfo = Resources.Load("Levels/testLevel") as TextAsset;
 
+        GameObject emptyObject = new GameObject();
+
         xmlDoc.LoadXml(levelinfo.text);
 
         string nameOfImage = xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[0].Attributes["source"].InnerText;
 
-        Debug.Log(nameOfImage);
+        //Debug.Log(nameOfImage);
 
         Sprite[] levelSprites = Resources.LoadAll<Sprite>(nameOfImage);
 
         XmlNodeList layerNames = xmlDoc.DocumentElement.GetElementsByTagName("layer");
 
         //get tile width and height\
-        float tileWidth = float.Parse(xmlDoc.DocumentElement.ChildNodes[0].Attributes["tileWidth"].InnerText);
-        float tileHeight = float.Parse(xmlDoc.DocumentElement.ChildNodes[0].Attributes["tileHeight"].InnerText);
+        float tileWidth = float.Parse(xmlDoc.DocumentElement.ChildNodes[0].Attributes["tilewidth"].InnerText);
+        float tileHeight = float.Parse(xmlDoc.DocumentElement.ChildNodes[0].Attributes["tileheight"].InnerText);
 
-        //go through first layer (background Floor layer)
-        float layerWidth = float.Parse(layerNames[0].Attributes["width"].InnerText);
-        float layerHeight = float.Parse(layerNames[0].Attributes["width"].InnerText);
-
-        //select data node
-        XmlNode dataNode = layerNames[0].SelectSingleNode("data");
-
-        //first (rows[0] and last (rows.Length-1) are empty
-        string[] rows = dataNode.InnerText.Split('\n');
-
-        for(int row = 1; row < layerHeight + 1; row++)
+        //doing all of this for each layer
+        for (int layerNum = 0; layerNum < layerNames.Count; layerNum++)
         {
-            string[] values = rows[row].Split(',');
-            for(int col = 0; col < layerWidth; col++)
-            {
+            //create object for layer 1
+            GameObject parentObject = new GameObject();
+            parentObject.name = layerNames[layerNum].Attributes["name"].InnerText;
 
+            //go through first layer (background Floor layer)
+            float layerWidth = float.Parse(layerNames[layerNum].Attributes["width"].InnerText);
+            float layerHeight = float.Parse(layerNames[layerNum].Attributes["width"].InnerText);
+
+            //select data node
+            XmlNode dataNode = layerNames[layerNum].SelectSingleNode("data");
+
+            //first (rows[0] and last (rows.Length-1) are empty
+            string[] rows = dataNode.InnerText.Split('\n'); //these are the rows, ex 275, 270, ...
+
+            for (int row = 1; row < layerHeight + 1; row++) //for each column
+            {
+                //getting the row based on the sprite
+                string[] values = rows[row].Split(','); // pull an entire row into individual values
+                for (int col = 0; col < layerWidth; col++)
+                {
+                    //
+                    int theIndexofSprite = int.Parse(values[col]);
+
+                    if (theIndexofSprite == 0)
+                        continue;
+
+                    GameObject tempSprite = Instantiate(emptyObject);
+                    tempSprite.transform.SetParent(parentObject.transform); // set the current object as a kid of the parent so it's collapsable in heirarchy
+                    tempSprite.transform.position = new Vector3(col, row, 0);
+                    SpriteRenderer tempRenderer = tempSprite.AddComponent<SpriteRenderer>();
+                    tempRenderer.sortingLayerName = parentObject.name;
+                    tempRenderer.sprite = levelSprites[theIndexofSprite - 1];
+
+                    //apply conponents based on layer
+                    if(parentObject.name == "Obstacle")
+                    {
+                        Rigidbody2D tempRB = tempSprite.AddComponent<Rigidbody2D>();
+                        tempRB.gravityScale = 0;
+                        tempRB.simulated = false;
+                        tempSprite.AddComponent<BoxCollider2D>();
+                    }
+                }
             }
         }
     }
